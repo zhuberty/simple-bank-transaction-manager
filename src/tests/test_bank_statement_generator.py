e805b8f75b8081014d4c0dfcff35ba225496d0f7
Zach Huberty <zachary.j.huberty@gmail.com>
2023-09-23 17:29:06 -0500

    add script to create mock bank statement

---
+++ src/tests/test_bank_statement_generator.py
@@ -0,0 +1,50 @@
+import csv
+import os
+from utils.create_mock_bank_statement import generate_bank_statement, write_to_csv  # Assuming your original code is in a file named "bank_statement_generator.py"
+
+def test_generate_bank_statement_num_entries():
+    num_entries = 500
+    data = generate_bank_statement(num_entries)
+    assert len(data) == num_entries
+
+def test_generate_bank_statement_chk_ref():
+    data = generate_bank_statement(1000)
+    missing_chk_ref = sum(1 for entry in data if not entry[1])
+    # It won't be exact due to randomness, but should be roughly 60%.
+    assert 550 < missing_chk_ref < 650
+
+def test_generate_bank_statement_debit_credit():
+    data = generate_bank_statement(1000)
+    debits = sum(1 for entry in data if entry[2])
+    credits = sum(1 for entry in data if entry[3])
+    assert debits + credits == 1000  # Make sure each entry has a debit or credit.
+
+def test_write_to_csv():
+    data = generate_bank_statement(10)
+    filename = "test_mock_bank_statement_remove.csv"
+    write_to_csv(filename, data)
+
+    # Check if file exists
+    assert os.path.exists(filename)
+    
+    # Clean up after test
+    os.remove(filename)
+
+def test_write_to_csv_no_overwrite():
+    data = generate_bank_statement(10)
+    filename = "./src/tests/test_data/test_mock_bank_statement_remove.csv"
+    write_to_csv(filename, data)
+
+    # Create a dummy entry
+    dummy_entry = generate_bank_statement(1)
+
+    # Attempt to write the dummy entry to the same file
+    write_to_csv(filename, dummy_entry)
+
+    # Check if the file contains only the original 10 entries and not the dummy entry
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        assert sum(1 for row in reader) == 11  # 10 entries + 1 header
+    
+    # Clean up after test
+    os.remove(filename)
---
+++ src/tests/utils/create_mock_bank_statement.py
@@ -0,0 +1,47 @@
+import csv
+import os
+from faker import Faker
+
+fake = Faker()
+
+def generate_bank_statement(num_entries=100):
+    statement = []
+    
+    for _ in range(num_entries):
+        account = fake.random_int(min=10000000, max=99999999)
+
+        # 60% chance of missing check reference
+        if fake.boolean(chance_of_getting_true=60):
+            chk_ref = ''
+        else:
+            chk_ref = fake.random_int(min=1000, max=9999)
+
+        debit = "{:.2f}".format(fake.random_int(min=1, max=500))
+        credit = "{:.2f}".format(fake.random_int(min=1, max=500))
+        date = fake.date_this_decade()
+        description = fake.company()
+
+        # Randomly choosing between debit and credit
+        if fake.boolean():
+            entry = (account, chk_ref, debit, '', date, description)
+        else:
+            entry = (account, chk_ref, '', credit, date, description)
+        
+        statement.append(entry)
+
+    return statement
+
+def write_to_csv(filename, data):
+    if os.path.exists(filename):
+        print(f"File '{filename}' already exists. No changes were made.")
+        return
+
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Account", "ChkRef", "Debit", "Credit", "Date", "Description"])
+        writer.writerows(data)
+
+
+if __name__ == '__main__':
+    statement = generate_bank_statement(1000)
+    write_to_csv("mock_bank_statement.csv", statement)
