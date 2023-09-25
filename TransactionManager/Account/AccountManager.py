a295c2aa50fd5cdddd65e137f273bc786d65c861
Zach Huberty <zachary.j.huberty@gmail.com>
2023-09-24 22:33:18 -0500

    Refactor and add additional placeholder classes.

---
+++ TransactionManager/Account/AccountManager.py
@@ -0,0 +1,4 @@
+class AccountManager:
+    """AccountManager class is responsible actions related to accounts."""
+    def __init__(self):
+        self.account_list = []
--- TransactionManager/AccountManager.py
+++
@@ -1,4 +0,0 @@
-class AccountManager:
-    """AccountManager class is responsible for managing all accounts in the system."""
-    def __init__(self):
-        self.account_list = []
