af95f86fc4cbc555b1a5719cd5c692259fc10f47
Zach Huberty <zachary.j.huberty@gmail.com>
2023-09-24 20:31:14 -0500

    Refactor main to work with test_data structure for now.

--- TransactionManager/main.py
+++ TransactionManager/main.py
@@ -1,13 +1,16 @@
+import os
+import logging
 from .csv_reader import read_csv
 from .categorizer import categorize
 from .report_generator import generate_report
 from .utils import get_calling_file_dir
 
+logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
+
 ACCOUNTS_DIRECTORY = get_calling_file_dir(__file__) + "/../tests/test_data/accounts/"
-DEFAULT_CSV_FILE = "mock_bank_statement.csv"
 
 def main():
-    file_path = CSV_DIRECTORY + DEFAULT_CSV_FILE
+    file_path = os.path.join(ACCOUNTS_DIRECTORY, "Test Bank Account Name/statements/mock_bank_statement.csv")
     transactions = read_csv(file_path)
     
     for transaction in transactions:
--- tests/test_read_csv.py
+++ tests/test_read_csv.py
@@ -1,11 +1,11 @@
 import os
+from TransactionManager.utils import get_calling_file_dir
 from TransactionManager.csv_reader import read_csv
 
 def test_read_csv():
     # get this file's directrory path
-    current_dir = os.path.dirname(os.path.realpath(__file__))
     # get the full path to the file
-    filepath = os.path.join(current_dir, "test_data/mock_bank_statement.csv")
+    filepath = os.path.join(get_calling_file_dir(__file__), "test_data/accounts/Test Bank Account Name/statements/mock_bank_statement.csv")
     transactions = read_csv(filepath)
     assert len(transactions) == 1000
     first_transaction = transactions[0]
