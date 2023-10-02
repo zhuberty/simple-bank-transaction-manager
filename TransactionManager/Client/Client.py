4147036d3bbdf83abde1003ccd9868e7ee763d1b
Zach Huberty <zachary.j.huberty@gmail.com>
2023-10-01 21:07:09 -0500

    Add button to clear admin console.

--- TransactionManager/Client/Client.py
+++ TransactionManager/Client/Client.py
@@ -9,33 +9,42 @@ class PageAdmin(tk.Frame):
         self.controller = controller
         self.MAX_CONOLE_LINES = 200
         self.errors = []
-        self.configure_check_dirs_btn()
         self.current_console_length = 0
         self.configure_console(height=10, width=80)
-
-
+        self.configure_check_dirs_btn()
+        self.configure_clear_console_btn()
 
     def configure_check_dirs_btn(self):
         self.check_directories_btn = tk.Button(
             self,
             text="Check Directories",
-            command=lambda: self.check_directories(),
+            command=lambda: self.configure_directories(),
         )
-        self.check_directories_btn.grid(row=1, column=0, columnspan=2)
+        self.check_directories_btn.grid(row=1, column=0, sticky="ew")
+
+    def configure_clear_console_btn(self):
+        self.clear_console_btn = tk.Button(
+            self, text="Clear Console", command=lambda: self.clear_console()
+        )
+        self.clear_console_btn.grid(row=1, column=1, sticky="ew")
 
     def configure_console(self, height, width):
         self.console = tk.Text(self, height=height, width=width, bg="black", fg="white")
         self.console.config(state="disabled")
-        self.console.grid(row=2, column=0, sticky="nsew")
+        self.console.grid(
+            row=2, column=0, columnspan=3, sticky="nsew"  # Change columnspan to 3
+        )
         self.configure_console_scrollbar()
         self.grid_rowconfigure(2, weight=1)
         self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=0)  # Set weight to 0
         self.log_message("Initialized application.")
 
     def configure_console_scrollbar(self):
         self.console_scrollbar = tk.Scrollbar(self, command=self.console.yview)
         self.console.config(yscrollcommand=self.console_scrollbar.set)
-        self.console_scrollbar.grid(row=2, column=1, sticky="ns")
+        self.console_scrollbar.grid(row=2, column=2, sticky="nse")  # Change column to 2
 
     def log_message(self, message):
         self.console.config(state="normal")
@@ -75,7 +84,7 @@ class PageAdmin(tk.Frame):
         else:
             self.log_message("Main directory exists.")
         return main_dir_exists
-    
+
     def create_main_dir(self):
         if not self.check_main_dir_exists():
             self.log_message("Creating Main directory...")
