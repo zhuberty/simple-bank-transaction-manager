83fcd74610b3a584106e6489e502b348c708223c
Zach Huberty <zachary.j.huberty@gmail.com>
2023-10-04 21:00:30 -0500

    Get and display mock transactions.

--- TransactionManager/ClientFrameMain.py
+++ TransactionManager/ClientFrameMain.py
@@ -1,6 +1,6 @@
 import tkinter as tk
+import pandas as pd
 from tkinter import filedialog
-from .ClientFrameAdmin import ClientFrameAdmin
 
 
 class ClientFrameMain(tk.Frame):
@@ -10,6 +10,7 @@ class ClientFrameMain(tk.Frame):
         self.configure_grid()
         self.configure_admin_btn()
         self.configure_import_file_btn()
+        self.configure_transactions_viewer()
 
     def configure_grid(self):
         self.grid(row=0, column=0, sticky="nsew")
@@ -32,6 +33,15 @@ class ClientFrameMain(tk.Frame):
             command=self.import_file_btn_event,
         )
         self.import_file_btn.grid(row=0, column=1, sticky="new")
+    
+    def configure_transactions_viewer(self):
+        self.transactions_viewer = tk.Text(
+            self,
+            height=10,
+            width=50,
+            state="disabled"
+        )
+        self.transactions_viewer.grid(row=1, column=0, columnspan=2, sticky="nsew")
 
     def import_file_btn_event(self):
         import_file_window = tk.Toplevel(self)
@@ -45,7 +55,10 @@ class ClientFrameMain(tk.Frame):
         select_file_btn.pack(pady=20)
 
     def open_file_dialog(self, import_file_window):
-        dialog_result = filedialog.askopenfilename()
+        dialog_result = filedialog.askopenfilename(
+            title="Select CSV File to Import",
+            filetypes=(("csv files", ".csv"), ("all files", "*.*"))
+        )
         self.open_file_dialog_callback(dialog_result, import_file_window)
 
     def open_file_dialog_callback(self, dialog_result, import_file_window):
@@ -54,3 +67,28 @@ class ClientFrameMain(tk.Frame):
         else:
             self.controller.frames["admin"].log_message("Error: File must be a csv file")
         import_file_window.destroy()
+
+    # read transactions csv into a dataframe
+    def read_transactions_csv(self, file_path):
+        try:
+            df = pd.read_csv(file_path)
+            return df
+        except Exception as e:
+            self.controller.frames["admin"].log_message("Error: " + str(e))
+            return None
+    
+    def display_transactions(self, df):
+        self.transactions_viewer.configure(state="normal")
+        self.transactions_viewer.delete("1.0", tk.END)
+        self.transactions_viewer.insert(tk.END, df.to_string())
+        self.transactions_viewer.configure(state="disabled")
+
+    def display_transactions_from_file(self, file_path):
+        df = self.read_transactions_csv(file_path)
+        if df is not None:
+            self.display_transactions(df)
+
+    def display_transactions_from_df(self, df):
+        if df is not None:
+            self.display_transactions(df)
+
--- tests/test_client_main.py
+++ tests/test_client_main.py
@@ -1,4 +1,8 @@
+import os
+from time import sleep
 import tkinter as tk
+import pandas as pd
+from TransactionManager.utils import get_cwd
 from TransactionManager.Client import Client
 
 class TestClientClientFrameMain:
@@ -6,6 +10,8 @@ class TestClientClientFrameMain:
     client = Client(test_main_dir)
     client.update_idletasks()
     page = client.frames["main"]
+    mock_transactions_filepath = os.path.join(get_cwd(__file__), "test_data/accounts/Test Bank Account Name/statements/mock_bank_statement.csv")
+    mock_transactions_df = pd.read_csv(mock_transactions_filepath)
 
     def test_init_frame(self):
         assert self.page.winfo_viewable()
@@ -21,7 +27,6 @@ class TestClientClientFrameMain:
         import_file_window = self.page.winfo_children()[-1]
         assert import_file_window.title() == "Import File"
         assert import_file_window.winfo_children()[0].cget("text") == "Select File"
-        # make sure file dialog closes
         import_file_window.destroy()
 
     def test_import_file_dialog_callback(self):
@@ -30,7 +35,14 @@ class TestClientClientFrameMain:
         self.client.frames["admin"].clear_console()
         self.page.open_file_dialog_callback("test_file.csv", import_file_window)
         assert self.client.frames["admin"].console.get("1.0", "1.end") == "Importing file: test_file.csv"
-        # assert throws error if the file is not a csv file
         self.client.frames["admin"].clear_console()
         self.page.open_file_dialog_callback("test_file.txt", import_file_window)
         assert self.client.frames["admin"].console.get("1.0", "1.end") == "Error: File must be a csv file"
+
+    def test_transactions_viewer_exists(self):
+        assert self.page.transactions_viewer.winfo_viewable()
+
+    def test_display_transactions(self):
+        self.page.display_transactions(self.mock_transactions_df)
+        self.client.update_idletasks()
+        assert self.page.transactions_viewer.get("1.0", "1.end")
