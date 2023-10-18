61b8c9c21d175b9f32fbffcf4d6db89761bca138
Zach Huberty <zhuberty@users.noreply.github.com>
2023-10-17 21:34:42 -0500

    Refactor console scrollbar.

--- TransactionManager/FrameAdmin.py
+++ TransactionManager/FrameAdmin.py
@@ -17,26 +17,15 @@ class FrameAdmin(Frame):
 
         self.console = Console(self)
         self.console.grid(row=2, column=0, columnspan=4, sticky="nsew")
-        self.configure_console_scrollbar()
+        self.console_scrollbar = WidgetHelper.create_scrollbar(self, self.console.widget.yview, 2, 2, "nse")
+        self.console.widget.config(yscrollcommand=self.console_scrollbar.set)
         self.log = self.console.log_message
         self.log("Initialized application.")
         
         self.clear_console_btn = WidgetHelper.create_button(self, "Clear Console", lambda: self.console.clear_console(), 1, 1, "ew")
         self.frame_main_btn = WidgetHelper.create_button(self, "Home", lambda: self.controller.main_frame.tkraise(), 1, 0, "ew")
 
-    def configure_console_scrollbar(self):
-        self.console_scrollbar = Scrollbar(self, command=self.console.widget.yview)
-        self.console.widget.config(yscrollcommand=self.console_scrollbar.set)
-        self.console_scrollbar.grid(row=2, column=3, sticky="nse")
-
     def configure_grid(self):
         self.grid_rowconfigure(2, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        WidgetHelper.configure_grid(self, 0, 2)
         self.grid_columnconfigure(2, weight=0)
-
-    def configure_console_scrollbar(self):
-        self.console_scrollbar = WidgetHelper.create_scrollbar(self, self.console.widget.yview, 2, 2, "nse")
-        self.console.widget.config(yscrollcommand=self.console_scrollbar.set)
-
-
