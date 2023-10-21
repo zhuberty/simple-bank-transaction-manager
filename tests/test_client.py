aa4c5b7655e148d1bfcee086c41f500e9c6cd3a2
Zach Huberty <10377254+zhuberty@users.noreply.github.com>
2023-10-20 19:36:54 -0500

    Remove geometry test.

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,9 +11,6 @@ class TestClient:
     def test_check_main_dir(self):
         assert self.client.main_dir is not None
 
-    def test_geometry_correct(self):
-        assert self.client.geometry().startswith("1200x800")
-
     def test_title_set(self):
         assert self.client.title() == "Simple Bank Transaction Manager"
 
